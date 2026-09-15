23fc3689a33ad66dbbed148c5702ffa2995495c6
SmileDev <longmoon2004@gmail.com>
2026-09-16 00:45:06 +0700

    Add menu item data structure and update main menu to use dynamic list

--- [lap]/lap_04/lib/mainMenu.dart
+++ [lap]/lap_04/lib/mainMenu.dart
@@ -11,9 +11,18 @@ import 'package:lap_04/exercises/exe_02.dart';
 import 'package:lap_04/exercises/exe_03.dart';
 import 'package:lap_04/exercises/exe_04.dart';
 import 'package:lap_04/exercises/exe_05.dart';
+import 'package:lap_04/mockdata/menuItemData.dart';
 
 class MainMenu extends StatelessWidget {
   const MainMenu({super.key});
+
+  final List<MenuItemData> menuList = const [
+    MenuItemData(title: 'Exercise 1: Core Widget Demo', screen: Exercise01()),
+    MenuItemData(title: 'Exercise 2: Input controls', screen: Exercise02()),
+    MenuItemData(title: 'Exercise 3: Layout', screen: Exercise03()),
+    MenuItemData(title: 'Exercise 4: App Structure', screen: Exercise04()),
+    MenuItemData(title: 'Exercise 5: Common UI', screen: Exercise05()),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,60 +38,49 @@ class MainMenu extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: ListView(
-          children: [
-            _buildMenu(
-              title: 'Exercise 1: Core Widget Demo',
+        child: ListView.builder(
+          itemCount: menuList.length,
+          itemBuilder: (context, index) {
+            final item = menuList[index];
+            return _buildMenu(
+              title: item.title,
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const Exercise01()),
+                  MaterialPageRoute(builder: (context) => item.screen),
                 );
               },
-            ),
-            _buildMenu(
-              title: 'Exercise 2: Input controls',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Exercise02()),
-                );
-              },
-            ),
-            _buildMenu(
-              title: 'Exercise 3: Layout',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Exercise03()),
-                );
-              },
-            ),
-            _buildMenu(
-              title: 'Exercise 4: App Structure',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Exercise04()),
-                );
-              },
-            ),
-            _buildMenu(
-              title: 'Exercise 5: Common UI',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Exercise05()),
-                );
-              },
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
   }
 
   Widget _buildMenu({required String title, required VoidCallback onTap}) {
-    return Container();
+    return Container(
+      child: Material(
+        color: Colors.white54,
+        child: ListTile(
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 6.0,
+          ),
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w900,
+              color: Colors.black87,
+            ),
+          ),
+          trailing: const Icon(
+            Icons.navigate_next_outlined,
+            color: Colors.black87,
+          ),
+          onTap: onTap,
+        ),
+      ),
+    );
   }
 }
